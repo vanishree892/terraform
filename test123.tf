794fbc50ff33e8aded2a30d946bedc00f9d741c9
vanishree892 <60698966+vanishree892@users.noreply.github.com>
2020-02-06 16:29:55 +0530

    Update test123.tf

--- test123.tf
+++ test123.tf
@@ -1,11 +1,45 @@
-provider "aws" {
-	region = "us-east-1"
-	access_key = "AKIARGEEKK37P4D2JSN3"
-	secret_key = "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"
+## Configure the vSphere Provider
+provider "vsphere" {
+    vsphere_server = "172.16.8.16"
+    user = "administrator@vpshere.local"
+    password = "Wipro@123"
+    allow_unverified_ssl = true
 }
 
+## Build VM
+data "vsphere_datacenter" "dc" {
+  name = "opendc-dc1"
+}
 
-resource "aws_instance" "sec" {
-			ami =  "ami-09d069a04349dc3cb"
-			instance_type = "t2.small"
+data "vsphere_datastore" "datastore" {
+  name          = "Local_datastore2_65.79"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {}
+
+data "vsphere_network" "mgmt_lan" {
+  name          = "VM Network2"
+ 
+}
+
+resource "vsphere_virtual_machine" "vanisid" {
+  name             = "vanisid"
+  resource_pool_id = "566"
+  datastore_id     = "1701"
+
+
+  num_cpus   = 1
+  memory     = 2048
+  wait_for_guest_net_timeout = 0
+  guest_id = "centos7_64Guest"
+  nested_hv_enabled =true
+
+
+  disk {
+   size             = 16
+   name             = "vanisid.vmdk"
+   eagerly_scrub    = false
+   thin_provisioned = true
+  }
 }
