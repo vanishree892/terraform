7101f0beb96e809085b81a07369123eba8836204
vanishree892 <60698966+vanishree892@users.noreply.github.com>
2020-02-17 14:37:51 +0530

    Update main1.tf

--- main1.tf
+++ main1.tf
@@ -1,4 +1,4 @@
 resource "local_file" "foo" {
     content     = "foo!"
-    filename = "https://github.com/vanishree892/terraform/test1111111111"
+    filename = "https://github.com/vanishree892/terraform/new/master/test1111111111"
 }
