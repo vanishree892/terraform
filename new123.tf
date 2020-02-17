c4ccf2ebb1984972bd1cc2802496742fd13f13cf
vanishree892 <60698966+vanishree892@users.noreply.github.com>
2020-02-17 14:28:49 +0530

    Update new123.tf

--- new123.tf
+++ new123.tf
@@ -1,27 +1,4 @@
-provider "vsphere" {
-  version              = "1.11.0"
-  user                 = "administrator@vpshere.local"
-  password             = "Wipro@123"
-  vsphere_server       = "172.16.8.16"
-  allow_unverified_ssl = true
-}
-
-resource "vsphere_virtual_machine" "myvm" {
-  name = "vm-deleteme"
-resource_pool_id = 566
-
- num_cpus = 2
-  memory   = 1024
- network_interface {
-    network_id = "VM Network"
-  }
- 
-
-  disk {
-   size             = 16
-   name             = "vanisid.vmdk"
-   eagerly_scrub    = false
-   thin_provisioned = true
-   
-  }
+resource "local_file" "foo" {
+    content     = "foo!"
+    filename = "https://github.com/vanishree892/terraform/testnew"
 }
